7a74041c8494fcd1931142b1e9c5f884ebf95a0f
Rishabh vishwakarma <101379213+81Rishabh@users.noreply.github.com>
2022-10-29 16:32:16 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(`${process.env.MONGODB_URI}`, {useNewUrlParser: true});
 
 db.on('error' , function(err) {
    console.log("Error in conneting to mongodb");
