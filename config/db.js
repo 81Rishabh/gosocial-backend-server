0f10e2f221b3b77783a4bc329a238e1ebd023c1f
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-10-29 17:00:40 +0530

    fix few changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/gosocial';
-mongoose.connect(url, {useNewUrlParser: true});
+const url = `${process.env.MONGODB_URI}` || 'mongodb://localhost:27017/gosocial';
+mongoose.connect(url);
 
 db.on('error' , function(err) {
    console.log("Error in conneting to mongodb");
