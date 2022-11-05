27db6a93f7eada4d383ed8d17a2e922a1a87c4ce
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-11-05 09:26:50 +0530

    chnage in db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.connect(`${process.env.MONGODB_URI}` || `${process.env.LOCAL}`, {
-  useFindAndModify: false,
-    useUnifiedTopology: true,
     useNewUrlParser: true,
 });
 const db = mongoose.connection;
